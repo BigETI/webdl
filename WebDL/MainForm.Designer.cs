2981cad7f819bb62e4f3cd719ce50d9a24cdaa08
Ethem Kurt <ethem-93@hotmail.de>
2016-05-15 21:58:43 +0200

    Initial commit.

---
+++ WebDL/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WebDL
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemTools = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemDownload = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel = new System.Windows.Forms.Panel();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.contextMenuStripBack = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timerBackForward = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.toolStripMenuItemTools});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(811, 24);
+            this.menuStrip.TabIndex = 0;
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemExit});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItemFile.Text = "File";
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(134, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
+            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            // 
+            // toolStripMenuItemTools
+            // 
+            this.toolStripMenuItemTools.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemDownload});
+            this.toolStripMenuItemTools.Name = "toolStripMenuItemTools";
+            this.toolStripMenuItemTools.Size = new System.Drawing.Size(47, 20);
+            this.toolStripMenuItemTools.Text = "Tools";
+            // 
+            // toolStripMenuItemDownload
+            // 
+            this.toolStripMenuItemDownload.Name = "toolStripMenuItemDownload";
+            this.toolStripMenuItemDownload.ShortcutKeys = System.Windows.Forms.Keys.F12;
+            this.toolStripMenuItemDownload.Size = new System.Drawing.Size(153, 22);
+            this.toolStripMenuItemDownload.Text = "Download";
+            this.toolStripMenuItemDownload.Click += new System.EventHandler(this.toolStripMenuItemDownload_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.textBoxURL);
+            this.panel.Controls.Add(this.buttonCancel);
+            this.panel.Controls.Add(this.buttonForward);
+            this.panel.Controls.Add(this.buttonBack);
+            this.panel.Controls.Add(this.buttonRefresh);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel.Location = new System.Drawing.Point(0, 24);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(811, 20);
+            this.panel.TabIndex = 1;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxURL.Location = new System.Drawing.Point(40, 0);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(731, 20);
+            this.textBoxURL.TabIndex = 8;
+            this.textBoxURL.Text = "http://www.google.de/";
+            this.textBoxURL.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxURL_KeyUp);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCancel.Location = new System.Drawing.Point(771, 0);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(20, 20);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "X";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonForward.Enabled = false;
+            this.buttonForward.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonForward.Location = new System.Drawing.Point(20, 0);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(20, 20);
+            this.buttonForward.TabIndex = 5;
+            this.buttonForward.Text = ">";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.ContextMenuStrip = this.contextMenuStripBack;
+            this.buttonBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonBack.Enabled = false;
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBack.Location = new System.Drawing.Point(0, 0);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(20, 20);
+            this.buttonBack.TabIndex = 3;
+            this.buttonBack.Text = "<";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonRefresh.Enabled = false;
+            this.buttonRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRefresh.Location = new System.Drawing.Point(791, 0);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(20, 20);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "R";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 44);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(811, 400);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            // 
+            // contextMenuStripBack
+            // 
+            this.contextMenuStripBack.Name = "contextMenuStripBack";
+            this.contextMenuStripBack.Size = new System.Drawing.Size(61, 4);
+            // 
+            // timerBackForward
+            // 
+            this.timerBackForward.Interval = 200;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 444);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "WebDL";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemTools;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemDownload;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripBack;
+        private System.Windows.Forms.Timer timerBackForward;
+    }
+}
+
---
+++ WebDL/MainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebDL
+{
+    public partial class MainForm : Form
+    {
+        private List<string> urls = new List<string>();
+        private List<string> urls_fw = new List<string>();
+        private bool append = true;
+        private bool no_fw = true;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            navigate(textBoxURL.Text);
+        }
+
+        private void navigate(string url, bool append = true, bool no_fw = true)
+        {
+            url = url.Trim();
+            int i = url.IndexOf("http://"), j = url.IndexOf("https://");
+            if ((i != 0) && (j != 0))
+                url = "http://" + url;
+            try
+            {
+                this.append = append;
+                this.no_fw = no_fw;
+                webBrowser.Url = new Uri(url);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Laufzeitfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void back()
+        {
+            if (urls.Any())
+            {
+                navigate(urls.Last(), false);
+            }
+        }
+
+        private void forward()
+        {
+            if (urls_fw.Any())
+            {
+                navigate(urls_fw.Last(), false, false);
+            }
+        }
+
+        private void rebuildContextMenuBack()
+        {
+            ToolStripMenuItem t;
+            List<string> l = new List<string>(urls);
+            l.Reverse();
+            int c = 0;
+            contextMenuStripBack.Items.Clear();
+            foreach (string i in l)
+            {
+                t = new ToolStripMenuItem();
+                t.Name = "urls_item" + c++;
+                t.Size = new System.Drawing.Size(95, 22);
+                t.Text = i;
+                t.Tag = i;
+                t.Click += new EventHandler(urls_item_Click);
+                contextMenuStripBack.Items.Add(t);
+            }
+        }
+
+        private void downloadFile(string url)
+        {
+            HttpClient c;
+            Task<Stream> t;
+            FileStream fs;
+            string ext;
+            webBrowser.Stop();
+            ext = Path.GetExtension(url).Trim('.');
+            if (ext.Length == 0)
+            {
+                saveFileDialog.Filter = "";
+                saveFileDialog.DefaultExt = "";
+            }
+            else
+            {
+                saveFileDialog.Filter = "\"" + ext + "\" File|*." + ext;
+                saveFileDialog.DefaultExt = ext;
+            }
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (fs = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        using (c = new HttpClient())
+                        {
+                            t = c.GetStreamAsync(url);
+                            t.Wait();
+                            t.Result.CopyTo(fs);
+                            t.Result.Dispose();
+                        }
+                    }
+                }
+                catch (Exception _e)
+                {
+                    MessageBox.Show(_e.Message, "Laufzeitfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void updateButtons()
+        {
+            rebuildContextMenuBack();
+            buttonBack.Enabled = urls.Any();
+            buttonForward.Enabled = urls_fw.Any();
+            buttonRefresh.Enabled = urls.Any();
+        }
+
+        private void toolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            back();
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            forward();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            string url = webBrowser.Url.ToString();
+            textBoxURL.Text = url;
+            if (urls.Any())
+            {
+                if (urls.Last() != url)
+                {
+                    if (append)
+                    {
+                        if (no_fw)
+                        {
+                            urls_fw.Clear();
+                            no_fw = false;
+                        }
+                        else
+                        {
+                            if (urls_fw.Any())
+                                urls_fw.RemoveAt(urls_fw.Count - 1);
+                        }
+                        urls.Add(url);
+                        append = false;
+                    }
+                    else
+                    {
+                        //urls_fw.Add(urls.Last());
+                        //urls.RemoveAt(urls.Count - 1);
+                    }
+                }
+            }
+            else
+            {
+                if (append)
+                {
+                    if (no_fw)
+                    {
+                        urls_fw.Clear();
+                        no_fw = false;
+                    }
+                    else
+                    {
+                        if (urls_fw.Any())
+                            urls_fw.RemoveAt(urls_fw.Count - 1);
+                    }
+                    urls.Add(url);
+                    append = false;
+                }
+            }
+            updateButtons();
+        }
+
+        private void toolStripMenuItemDownload_Click(object sender, EventArgs e)
+        {
+            downloadFile(textBoxURL.Text);
+        }
+
+        private void textBoxURL_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                navigate(textBoxURL.Text);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            webBrowser.Stop();
+        }
+
+        private void urls_item_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem t;
+            if (sender is ToolStripMenuItem)
+            {
+                t = (ToolStripMenuItem)sender;
+                navigate((string)(t.Tag), false);
+            }
+        }
+    }
+}
